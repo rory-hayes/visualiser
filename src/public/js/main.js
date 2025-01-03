2bf64b8195b038489e270f2a6bd16e749f553b38
rory-hayes <54278439+rory-hayes@users.noreply.github.com>
2025-01-03 23:17:54 +0000

    homepage updates

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -3,12 +3,14 @@ import { Search } from './modules/search.js';
 import { Filters } from './modules/filters.js';
 import { Graph } from './modules/graph.js';
 import { Stats } from './modules/stats.js';
+import { initializePreviewGraph } from './preview-graph.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     const isLandingPage = window.location.pathname === '/';
 
     if (isLandingPage) {
         initializeLandingPage();
+        initializePreviewGraph();
     } else {
         initializeDashboard();
     }
